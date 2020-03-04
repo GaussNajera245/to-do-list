4c93a1b21dc6d3a159b9c0b1a012ff29f079a122
GusNajera <gvt_a9@hotmail.com>
2020-03-04 10:48:41 -0600

    deploying with another branch that contains the build

---
+++ precache-manifest.13a9320b5141b5bd2aba171e7e5ad682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b46de7e91e8bed5fc95b79b7e7e40d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d908938395f85e84d8e2",
+    "url": "/static/css/main.ad0aa30e.chunk.css"
+  },
+  {
+    "revision": "992e90eb89427d228599",
+    "url": "/static/js/2.5d46b4c3.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.5d46b4c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d908938395f85e84d8e2",
+    "url": "/static/js/main.a1dda44f.chunk.js"
+  },
+  {
+    "revision": "57f13d33536b6b4c3427",
+    "url": "/static/js/runtime-main.c98aac36.js"
+  }
+]);
